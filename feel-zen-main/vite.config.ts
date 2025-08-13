f7cd0298d6b45f1aef151543747a67cbf3b0d996
devendra446 <75429541+devendra446@users.noreply.github.com>
2025-08-13 12:16:34 +0530

    Update vite.config.ts

--- feel-zen-main/vite.config.ts
+++ feel-zen-main/vite.config.ts
@@ -2,23 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  base: "./", // <-- important for correct asset paths on Vercel
-}));
+});
+
 
